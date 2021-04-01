dfb285b88ed9ea3373f4c7ada137271ffb39f47b
Ryan Lawrence <volpanicstudios@gmail.com>
2021-04-01 16:36:06 +1100

    Added a script and post process to prevent the player from exiting the main area. Added a millrose terminal model prefab.

---
+++ ShaderGraphProject/Assets/ExitPlayfield.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitPlayfield : MonoBehaviour
+{
+    private Vector3 respawnPos;
+    private Vector3 rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject obj = GameObject.FindGameObjectWithTag("Player");
+
+        //Store startup varaibles to reset when player has left the play field
+        respawnPos = obj.transform.position;
+        rotation = obj.transform.eulerAngles;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.transform.position = respawnPos;
+            other.gameObject.transform.eulerAngles = rotation;
+        }
+    }
+}
